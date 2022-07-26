f3c8ab70efa91e1f2d205e60e8b8a897eb411706
Cory <cjbenicak@gmail.com>
2022-07-26 10:17:48 -0400

    Created pages and components in react

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,7 +1,23 @@
 import './styles/App.css'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Nav from './pages/Nav'
+import Blog from './components/Blog'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <header>
+        <Nav />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blog" element={<Blog />} />
+        </Routes>
+      </main>
+    </div>
+  )
 }
 
 export default App
---
+++ blog/src/components/Blog.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom'
+
+const Blog = () => {
+  return(
+<div>
+  <nav>
+    <Link to='/topics'>Topics</Link>
+  </nav>
+
+</div>
+  )
+}
+
+export default Blog
