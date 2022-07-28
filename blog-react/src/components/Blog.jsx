ad3d91c4a9faf84a773149298e8a452cced57bdb
Cory <cjbenicak@gmail.com>
2022-07-28 16:50:53 -0400

    logic started for created blog divs with onClick attached

--- blog-react/src/components/Blog.jsx
+++ blog-react/src/components/Blog.jsx
@@ -1,18 +1,22 @@
-import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Blog = (props) => {
 
-
+let navigate = useNavigate()
 const newBlogs = props.newBlogs
 
 
-  
+const showBlog = (blog) => {
+  navigate(`${blog.id}`)
+}
+
 
 
   return(
 <div className='blogComp'>
+  
 {newBlogs.map((blog) => (
-<div >
+<div onClick={() => showBlog(blog)} key={blog.id} >
   <h1>Blogs</h1>
   <h3>title:{blog.title}</h3>
   <h3>topic:{blog.topic}</h3>
