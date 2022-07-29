8b57ad016d0480c274877048567be53175e049dd
Cory <cjbenicak@gmail.com>
2022-07-29 11:11:44 -0400

    styling for h1 in create comp

--- blog-react/src/components/Create.jsx
+++ blog-react/src/components/Create.jsx
@@ -1,6 +1,5 @@
 import Form from './Form'
-import { useState } from 'react'
-import axios from 'axios'
+
 const BASE_URL = 'http://localhost:3001/api'
 
 const Create = (props) => {
@@ -9,7 +8,7 @@ const Create = (props) => {
   return(
     <div className='create'>
       <header>
-        <h1> Whats on your mind?</h1>
+        <h1 className='createTitle'> Whats on your mind?</h1>
       </header>
       <div>
         <Form handleChange={props.handleChange} handleSubmit={props.handleSubmit} formState={props.formState}/>
